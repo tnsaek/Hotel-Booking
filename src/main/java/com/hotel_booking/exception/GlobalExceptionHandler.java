2986ee4d646a80714fdf091a25069807559f7446
Brhane <tnsu32@gmail.com>
2026-04-05 17:31:01 -0400

    add custom exception

---
+++ src/main/java/com/hotel_booking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.hotel_booking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+}
